a969b7d6570cbc84ca2f1c3cd16493dbc1b6b2e0
Radium Zheng <ztl8702@users.noreply.github.com>
2019-12-20 16:03:57 +1100

    basic js lib

---
+++ examples/js/app.ts
@@ -0,0 +1,7 @@
+import {YngPingTwoSyllable} from "@hokchewjs/lib/models/yngping/yngping-two";
+import { parseHandwriting } from "@hokchewjs/lib/models/yngping/yngping-two";
+
+console.log("Then use it");
+console.log(parseHandwriting("wa", null));
+
+//èi/ güong huà lǘ uāng wa`
